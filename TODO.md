131adaa2ece6b74cef794d01925060075e0e7183
R. S. Doiel <rsdoiel@gmail.com>
2015-08-20 09:39:22 -0700

    Basic features implemented-- append, prepend, clip

--- TODO.md
+++ TODO.md
@@ -2,12 +2,3 @@
 # Someday, maybe
 
 + think about renaming prepend (verb) to prefix (used as a verb)
-+ clip function would find the path in current environment variable and remove it
-+ rearrange path to force a particular subpath to the front or back
-    + action sequence
-        + clip then either prepend or append as desired
-    + use cases
-        + For PATH="/bin:/usr/bin:/usr/local/bin" modify it to have /usr/local/bin first
-            + mergepath --prefix /usr/local/bin
-        + For PATH="/bin:/usr/bin:/usr/local/bin" modify to have /usr/bin last
-            + mergepath --append /usr/bin
--- mergepath.go
+++ mergepath.go
@@ -9,27 +9,28 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "flag"
-    "strings"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
 )
 
 var (
-    envPath string
-    dir string
-    help bool
-    appendPath = true
-    prependPath = false
+	envPath     string
+	dir         string
+	help        bool
+	appendPath  = true
+	prependPath = false
+	clipPath    = false
 )
 
 var usage = func(exit_code int, msg string) {
-    var fh = os.Stderr
+	var fh = os.Stderr
 
-    if exit_code == 0 {
-        fh = os.Stdout
-    }
-    fmt.Fprintf(fh, `%s
+	if exit_code == 0 {
+		fh = os.Stdout
+	}
+	fmt.Fprintf(fh, `%s
  USAGE %s [OPTIONS] PATH_TO_ADD PATH_TO_MODIFY
 
  EXAMPLES
@@ -40,67 +41,84 @@ var usage = func(exit_code int, msg string) {
  OPTIONS
 
 `, msg, os.Args[0], os.Args[0], os.Args[0])
-    flag.VisitAll(func (f *flag.Flag) {
-        fmt.Fprintf(fh, "\t-%s\t(defaults to %s) %s\n", f.Name, f.DefValue, f.Usage)
-    })
-    fmt.Fprintf(fh, `
+	flag.VisitAll(func(f *flag.Flag) {
+		fmt.Fprintf(fh, "\t-%s\t(defaults to %s) %s\n", f.Name, f.DefValue, f.Usage)
+	})
+	fmt.Fprintf(fh, `
 
  Copyright (c) 2014 All rights reserved.
  Released under the Simplified BSD License
  See: http://opensource.org/licenses/bsd-license.php
 `)
-    os.Exit(exit_code)
+	os.Exit(exit_code)
 }
 
 func init() {
-    const (
-        pathUsage = "The path you want to merge with."
-        dirUsage = "The directory you want to add to the path."
-        appendUsage = "If directory is not in path, append the directory to the path"
-        prependUsage = "If directory is not in path, prepend the directory to the path"
-        helpUsage = "This help document."
-    )
-    envPath = os.Getenv("PATH")
-    flag.StringVar(&envPath, "path", envPath, pathUsage)
-    flag.StringVar(&envPath, "p", envPath, pathUsage)
-    flag.StringVar(&dir, "directory", dir, dirUsage)
-    flag.StringVar(&dir, "d", dir, dirUsage) 
+	const (
+		pathUsage    = "The path you want to merge with."
+		dirUsage     = "The directory you want to add to the path."
+		appendUsage  = "Append the directory to the path, moving it was already in the path"
+		prependUsage = "Prepend the directory to the path, moving it was already in the path"
+		clipUsage    = "Remove a directory from the path"
+		helpUsage    = "This help document."
+	)
+	envPath = os.Getenv("PATH")
+	flag.StringVar(&envPath, "path", envPath, pathUsage)
+	flag.StringVar(&envPath, "p", envPath, pathUsage)
+	flag.StringVar(&dir, "directory", dir, dirUsage)
+	flag.StringVar(&dir, "d", dir, dirUsage)
 
-    flag.BoolVar(&appendPath, "append", appendPath, appendUsage)
-    flag.BoolVar(&appendPath, "a", appendPath, appendUsage)
-    flag.BoolVar(&prependPath, "prepend", prependPath, prependUsage)
-    flag.BoolVar(&prependPath, "P", prependPath, prependUsage)
-    flag.BoolVar(&help, "help", false, helpUsage)
-    flag.BoolVar(&help, "h", false, helpUsage)
+	flag.BoolVar(&appendPath, "append", appendPath, appendUsage)
+	flag.BoolVar(&appendPath, "a", appendPath, appendUsage)
+	flag.BoolVar(&prependPath, "prepend", prependPath, prependUsage)
+	flag.BoolVar(&prependPath, "P", prependPath, prependUsage)
+	flag.BoolVar(&clipPath, "clip", clipPath, clipUsage)
+	flag.BoolVar(&clipPath, "c", clipPath, clipUsage)
+	flag.BoolVar(&help, "help", false, helpUsage)
+	flag.BoolVar(&help, "h", false, helpUsage)
+}
+
+func clip(envPath string, dir string) string {
+	oParts := []string{}
+	iParts := strings.Split(envPath, ":")
+	for _, v := range iParts {
+		if v != dir {
+			oParts = append(oParts, v)
+		}
+	}
+	return strings.Join(oParts, ":")
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    if help == true {
-        usage(0, "")
-    }
+	if help == true {
+		usage(0, "")
+	}
 
-    if flag.NArg() > 0 {
-        dir = flag.Arg(0)
-        if flag.NArg() == 2 {
-            envPath = flag.Arg(1)
-        }
-    }
+	if flag.NArg() > 0 {
+		dir = flag.Arg(0)
+		if flag.NArg() == 2 {
+			envPath = flag.Arg(1)
+		}
+	}
 
-    if dir == "" {
-        usage(1, "Missing directory to add to path")
-    }
-    if prependPath == true {
-        appendPath = false
-    }
-    if strings.Contains(envPath, dir) {
-        fmt.Println(envPath)
-        os.Exit(0)
-    }
-    if appendPath == true {
-        fmt.Printf("%s:%s", envPath, dir)
-    } else {
-        fmt.Printf("%s:%s", dir, envPath)
-    }
+	if dir == "" {
+		usage(1, "Missing directory to add to path")
+	}
+	if clipPath == true {
+		fmt.Printf("%s", clip(envPath, dir))
+		os.Exit(0)
+	}
+	if prependPath == true {
+		appendPath = false
+	}
+	if strings.Contains(envPath, dir) {
+		envPath = clip(envPath, dir)
+	}
+	if appendPath == true {
+		fmt.Printf("%s:%s", envPath, dir)
+	} else {
+		fmt.Printf("%s:%s", dir, envPath)
+	}
 }
